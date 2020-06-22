a53dc846bc863b42ebf335b0ca452c5c78e6427e
Gustavo Mendonça <gustavohm.mendonca@gmail.com>
2020-06-22 10:18:26 -0300

    First commit

---
+++ src/Server.ts
@@ -0,0 +1,50 @@
+import {PlatformApplication, Configuration, Inject} from "@tsed/common";
+// import {GlobalAcceptMimesMiddleware} from "@tsed/platform-express";
+import * as bodyParser from "body-parser";
+import * as compress from "compression";
+import * as cookieParser from "cookie-parser";
+import * as methodOverride from "method-override";
+import * as cors from "cors";
+import "@tsed/ajv";
+import "@tsed/typeorm";
+import typeormConfig from "./config/typeorm";
+
+const rootDir = __dirname;
+
+@Configuration({
+  rootDir,
+  acceptMimes: ["application/json"],
+  httpPort: process.env.PORT || 8083,
+  httpsPort: false, // CHANGE
+  mount: {
+    "/rest": [
+      `${rootDir}/controllers/**/*.ts`
+    ]
+  },
+  // typeorm: typeormConfig,
+  exclude: [
+    "**/*.spec.ts"
+  ]
+} as any)
+export class Server {
+  @Inject()
+  app: PlatformApplication;
+
+  @Configuration()
+  settings: Configuration;
+
+  $beforeRoutesInit() {
+    this.app
+      .use(cors())
+      // .use(GlobalAcceptMimesMiddleware)
+      .use(cookieParser())
+      .use(compress({}))
+      .use(methodOverride())
+      .use(bodyParser.json())
+      .use(bodyParser.urlencoded({
+        extended: true
+      }));
+
+    return null;
+  }
+}
